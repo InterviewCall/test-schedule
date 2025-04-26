ee64d3cac9db7bfd6c803bc9b3ef9d1de2a55328
Arijit Ganguly <aganguly158@gmail.com>
2025-04-27 01:30:07 +0530

    siteconfig updated

---
+++ src/constants/config.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+    title: 'InterviewCall Test Schedule Portal',
+    description:
+      'Manage and schedule tests efficiently with InterviewCall Test Schedule Portal.',
+    /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
+    url: 'https://www.schedule.interviewcall.club',
+};
