bd36d552d4547ef489ce1229a1989cb8503e0734
Arijit Ganguly <aganguly158@gmail.com>
2025-04-29 00:25:41 +0530

    format date logic changed

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 import { subMinutes } from 'date-fns';
-import { toZonedTime } from 'date-fns-tz';
+import { fromZonedTime, toZonedTime } from 'date-fns-tz';
 
 export const formatDate = (date: Date) => {
   return new Intl.DateTimeFormat('en-GB', {
@@ -20,14 +20,8 @@ export const formatTime = (time: Date) => {
 };
 
 export const getMaxStartTime = (time: Date) => {
-  // const zonedTime = toZonedTime(new Date(time), 'Asia/Kolkata');
-  const adjustedTime = subMinutes(time, 22);
-  const maxStartTime = toZonedTime(adjustedTime, 'Asia/Kolkata');
-
-  return Intl.DateTimeFormat('en-GB', {
-    hour: 'numeric',
-    minute: '2-digit',
-    hour12: true,
-    timeZone: 'Asia/Kolkata',
-  }).format(maxStartTime);
+  const zonedTime = toZonedTime(new Date(time), 'Asia/Kolkata');
+  const adjustedTime = subMinutes(zonedTime, 22);
+  
+  return fromZonedTime(adjustedTime, 'Asia/Kolkata');
 };
