8bf1437964da06f90dd9ea67d0356b3d54d45788
Arijit Ganguly <aganguly158@gmail.com>
2025-04-29 00:19:11 +0530

    format date logic changed

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -20,8 +20,8 @@ export const formatTime = (time: Date) => {
 };
 
 export const getMaxStartTime = (time: Date) => {
-  const zonedTime = toZonedTime(new Date(time), 'Asia/Kolkata');
-  const adjustedTime = subMinutes(zonedTime, 22);
+  // const zonedTime = toZonedTime(new Date(time), 'Asia/Kolkata');
+  const adjustedTime = subMinutes(time, 22);
   const maxStartTime = toZonedTime(adjustedTime, 'Asia/Kolkata');
 
   return Intl.DateTimeFormat('en-GB', {
