285f19dd5eb157cff5fcafeff3c9501cba373656
Arijit Ganguly <aganguly158@gmail.com>
2025-10-07 22:35:45 +0530

    #modify: modified invited by options

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,14 +1,10 @@
 import { OptionType } from '@/types';
 
 export const advisorOptions: OptionType[] = [
-    { value: 'Prithviraj Paul', label: 'Prithviraj Paul' },
     { value: 'Sayan Das', label: 'Sayan Das' },
-    { value: 'Priyanka Das', label: 'Priyanka Das' },
-    { value: 'Navneet Poddar', label: 'Navneet Poddar' },
     { value: 'M Mandal', label: 'M Mandal' },
     { value: 'Anjali', label: 'Anjali' },
-    { value: 'Sohail', label: 'Sohail' },
     { value: 'Setu', label: 'Setu' },
-    { value: 'Ashish', label: 'Ashish' },
     { value: 'Subhajit', label: 'Subhajit' },
+    { value: 'Srinjoy', label: 'Srinjoy' },
 ];
