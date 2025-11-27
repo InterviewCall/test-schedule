c5f35a1d4c45602cccfea5abff72d027d2f58869
Arijit Ganguly <aganguly158@gmail.com>
2025-11-27 13:26:47 +0530

    #modify: modified advisor options

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,9 @@
 import { OptionType } from '@/types';
 
 export const advisorOptions: OptionType[] = [
-    { value: 'Sayan Das', label: 'Sayan Das' },
     { value: 'M Mandal', label: 'M Mandal' },
     { value: 'Anjali', label: 'Anjali' },
     { value: 'Setu', label: 'Setu' },
     { value: 'Subhajit', label: 'Subhajit' },
-    { value: 'Srinjoy', label: 'Srinjoy' },
+    { value: 'Piyal Dutta', label: 'Piyal Dutta' }
 ];
