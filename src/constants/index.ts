d8931be0facf626a267f36057d5a797bf8e6580d
Arijit Ganguly <aganguly158@gmail.com>
2026-04-22 15:49:27 +0530

    #modify: modified all the packages and added a new advisor

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,14 +1,14 @@
 import { OptionType } from '@/types';
 
 export const advisorOptions: OptionType[] = [
-    { value: 'M Mandal', label: 'M Mandal' },
-    { value: 'Anjali', label: 'Anjali' },
+    // { value: 'M Mandal', label: 'M Mandal' },
+    // { value: 'Anjali', label: 'Anjali' },
     { value: 'Setu', label: 'Setu' },
-    { value: 'Sayan', label: 'Sayan' },
+    // { value: 'Sayan', label: 'Sayan' },
     { value: 'Subhajit', label: 'Subhajit' },
     // { value: 'Piyal', label: 'Piyal' },
-    { value: 'Dutta', label: 'Dutta' },
-    { value: 'Mahima', label: 'Mahima' },
+    // { value: 'Dutta', label: 'Dutta' },
+    { value: 'Devtanaya', label: 'Devtanaya' },
     { value: 'Ashish Chauhan', label: 'Ashish Chauhan' },
 ];
 
